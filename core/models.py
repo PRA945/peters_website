8cc59f91c70ce009d0904e450f6e9ce38d460204
Peter <pannunziata945@gmail.com>
2015-10-27 07:09:13 +0000

    Add question create view

--- core/models.py
+++ core/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Question(models.Model):
+  Name = models.CharField(max_length=300)
+  Email = models.CharField(max_length=300)
+  Message = models.TextField(null=True, blank=True)
+  created_at = models.DateTimeField(auto_now_add=True)
+
+ 
+    
